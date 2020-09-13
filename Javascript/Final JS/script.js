eed19d696ae209b2a25b725eccdf97466cccf1b5
ArtART241 <graubergerartem@gmail.com>
2020-09-13 14:01:17 +0200

    lesson 13.09.20

--- Javascript/Final JS/script.js
+++ Javascript/Final JS/script.js
@@ -4,21 +4,30 @@ function onSubmit(event) {
     event.preventDefault();
 
     let form = event.target;
-    let name = event.target.userName.value;
+    let name = form.userName.value;
     let age = form.age.value;
     let from = form.check.value;
-    // in development
-    let like = form.userLike.value;
 
-    console.log("name: " + name);
-    console.log("age: " + age);
-    console.log("from: " + from);
     // in development
-    console.log("like: " + like);
+
+
 
     let s = "Once in a beautiful world " + name + " went to grandma to get some cash. Even if he was " + age + " years old and came from " + from + ", it was worth it, because grandma had a lot of money. ";
     story.innerHTML = s;
 
+    // got elements from checkboxes
+    let userLike = document.getElementsByClassName("userLike");
+    let like = [];
+
+    for (i = 0; i < userLike.length; i++) {
+        if (userLike[i].checked == true) {
+            like.push(userLike[i].value)
+           
+            
+        }
+       
+    } 
+    console.log(like);
 }
 
 popitka.addEventListener("submit", onSubmit);
---
+++ jQuery/1 jQuery/script.js
@@ -0,0 +1,3 @@
+
+
+console.log($(".circle").css("background-color"))
