752ded59b527b2f2b09c3b0e37de01f98fab9f79
ArtART241 <graubergerartem@gmail.com>
2020-10-10 12:11:22 +0200

    updated to newest progress

--- jQuery/2 jQuery/script.js
+++ jQuery/2 jQuery/script.js
@@ -1,51 +1,91 @@
+// ************** Параметры игры ******************
 
-// create elements HTML using jQuery
-let img = $("<img>");
-
-// set one attribute
-$(img).attr("src", "img/03.png");
-
-// appending to body
-$("body").append(img);
-
-let rubashka = "img/01.png"
-
-function generateField(size) {
+// Готовый алгоритм перебора карт
+function shuffle(array) {
+    let copy = [], n = array.length, i;
   
-    // resolution 150x²
-    let gamesize = size * 150 + 16;
-    let place = (innerWidth - gamesize) / 2;
-
-    let cards = new Array(size*size)
-    cards.fill( $("<img>").attr({
-        "src": rubashka,
-        "width": "150",
-        "class": "card",
-        "id": "rub"
-    }))
-    
-    let bigsize = cards.length * 150 + 16;
-    
-    $("#field").append(cards);
-    
-
-
-    console.log(cards);
+    // While there remain elements to shuffle…
+    while (n) {
+  
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * array.length);
+  
+      // If not already shuffled, move it to the new array.
+      if (i in array) {
+        copy.push(array[i]);
+        delete array[i];
+        n--;
+      }
+    }
+  
+    return copy;
 }
 
-
-// click event
-$("#btn").click(function (event) { 
-   
-    // getting input 'size' from user
-    let size = $("#size").val();
-    // checking, if it can be divided by 2
-    if (size % 2 == 0) {
-
-        console.log("nice");
-
-    } else {
-
-        console.log("nicht so tollè");
+// Функция генерации игрового поля
+function generateField(size){
+    
+    let cls = ["c1", "c2", "c3", "c4"]
+    let counter = 0;
+    let backs = [];
+    for(let i=0; i<(size*size); i+=2){
+        backs[i] = cls[counter];
+        backs[i+1] = cls[counter];
+        
+        if(counter < 3){
+            counter++;
+        }
+        else counter = 0;
     }
+
+    console.log(backs)
+
+    let cards = [];
+    for(let i=0; i<(size*size); i++){
+        let card = $("<div>");
+        $(card).addClass("card");
+
+        let flipper = $("<div>");
+        $(flipper).addClass("flipper");
+
+        let front = $("<div>");
+        $(front).addClass("front");
+
+        let back = $("<div>");
+        $(back).addClass("back");
+
+        $(flipper).append(back)
+        $(flipper).prepend(front)
+        $(card).append(flipper)
+
+        cards.push(card);
+    }
+    // Обнуляем поле
+    $("#field").html("");
+    // Устанавливаем размер и местоположение поля
+    $("#field").css({
+        "width": size*150+16 + "px",
+        "margin-left": (window.innerWidth - size*150+16)/2 + "px"
+    })
+    // Добавляем в наше поле карточки
+    $("#field").append(cards);
+}
+
+// Нажатие 
+$("#btn").click(function(event) { 
+    // Взять значение из #size используя .val()
+    size = $("#size").val();
+    if(size%2 == 0){
+        generateField(size);
+    }
+    else{
+        alert("Число не может быть нечетным!!!")
+    }
+});
+
+
+
+// Переворот картинки при нажатии
+$(".flipper").click(function(event){ 
+    $(event.currentTarget).toggleClass("clicked");
+    
 });
